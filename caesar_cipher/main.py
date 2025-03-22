8a325364d459d18a8d69d38bb21448e3f00956af
JideMartins <jeedeyy@gmail.com>
2025-03-22 00:46:34 -0400

    Separate Alphabet list - new file:   alphabets.py - modified:   main.py Added an alphabets file for future use and updates

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -1,3 +1,4 @@
+from alphabets import alphabet
 from ascii import logo
 
 print(logo)
@@ -28,11 +29,9 @@ loop_code = True
 
 while loop_code:
 
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
     direction = input(
         "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
