9e9390df14210fbb05d1e4e27bc6c7cad47776ad
JideMartins <jeedeyy@gmail.com>
2025-03-22 00:40:28 -0400

    Add loop feature

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -1,12 +1,3 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input(
-    "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
 # Combine both the encrypt and decrypt functions
 def caesar(original_text, shift_amount, cipher_direction):
     output_text = ""
@@ -28,4 +19,22 @@ def caesar(original_text, shift_amount, cipher_direction):
     print(f"Here is your {cipher_direction}d text: {output_text}")
 
 
-caesar(text, shift, direction)
+loop_code = True
+
+while loop_code:
+
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    direction = input(
+        "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(text, shift, direction)
+
+    yes_or_no = input(
+        "Type \"yes\" if you want to go again or \"n\" if you want to end program\n").lower()
+
+    if yes_or_no == "no":
+        loop_code = False
