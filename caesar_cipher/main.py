63be6887b9ff51bfc78dffd3aa900ef2b3679924
JideMartins <jeedeyy@gmail.com>
2025-03-22 00:29:20 -0400

    Format file

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -1,8 +1,11 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+direction = input(
+    "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
 
 def encrypt(original_text, shift_amount):
     encoded_text = ""
@@ -16,9 +19,12 @@ def encrypt(original_text, shift_amount):
     print(f"original text: {original_text}")
     print(f"encrypted text is {encoded_text}")
 
+
 encrypt(text, shift)
 
 # Define decrypt function
+
+
 def encrypt(original_text, shift_amount):
     decoded_text = ""
     for char in original_text:
