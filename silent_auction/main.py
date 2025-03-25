2476b2a966585d0001b4a683cac21f2e52252116
JideMartins <jeedeyy@gmail.com>
2025-03-24 22:04:52 -0400

    Get maximum and print

--- silent_auction/main.py
+++ silent_auction/main.py
@@ -14,11 +14,17 @@ while continue_bid:
     bid[name] = price
 
     # check if there are other bidders
-    other_bidders = input("Are there other bidders? Type \"y\" for yes and \"n\" for no: ").lower()
+    other_bidders = input(
+        "Are there other bidders? Type \"y\" for yes and \"n\" for no: ").lower()
 
     if other_bidders == "n":
         continue_bid = False
-    
+
     print(bid)
 
 
+# Get max price and key
+max_name = max(bid, key=bid.get)
+max_price = max(bid.values())
+
+print(f"The winner is {max_name} with a bid of ${max_price}")
