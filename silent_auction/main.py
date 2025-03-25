e1da0590090435266b2e114cb53f80be9fbaf980
JideMartins <jeedeyy@gmail.com>
2025-03-24 21:50:36 -0400

    Add loop business logic

--- silent_auction/main.py
+++ silent_auction/main.py
@@ -1,10 +1,24 @@
-
 bid = {}
 
-# Ask for inputs
-name = input("Enter your name:\n")
-price = input("Enter your bidding price:\n$")
 
-# check if there are other bidders
-other_bidders = input("Are there other bidders?").lower()
+# declare variable for continuing bid
+continue_bid = True
+
+while continue_bid:
+
+    # Ask for inputs
+    name = input("Enter your name:\n")
+    price = int(input("Enter your bidding price:\n$ "))
+
+    # populate bid
+    bid[name] = price
+
+    # check if there are other bidders
+    other_bidders = input("Are there other bidders? Type \"y\" for yes and \"n\" for no: ").lower()
+
+    if other_bidders == "n":
+        continue_bid = False
+    
+    print(bid)
+
 
