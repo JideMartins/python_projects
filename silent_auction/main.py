634d67e3e7575c7a16e37565ca70c663cc949b2e
JideMartins <jeedeyy@gmail.com>
2025-03-24 23:53:44 -0400

    Fix bugs - The code stopped running when it goes for the second time. -  This is because the `auction()` function is called and it stops just there instead of looping. The code was moced into the function.

--- silent_auction/main.py
+++ silent_auction/main.py
@@ -1,13 +1,17 @@
+# import logo
 from gavel import logo
 
 print(logo)
 
-bid = {}
-
+# define function
 
 def auction():
+    # declare empty dict
+    bid = {}
     # declare variable for continuing bid
     continue_bid = True
+
+    # loop
     while continue_bid:
 
         # Ask for inputs
@@ -21,31 +25,34 @@ def auction():
         other_bidders = input(
             "Are there other bidders? Type \"y\" for yes and \"n\" for no: ").lower()
 
+        
         if other_bidders == "n":
             continue_bid = False
-        elif other_bidders == "y":
-            continue
-        else:
+            # Get max price and key
+            max_name = max(bid, key=bid.get)
+            max_price = max(bid.values())
+
+            # convert bid values to list
+            max_count = list(bid.values()).count(max_price)
+
+
+            if max_count == 1:
+                print(f"The winner is {max_name} with a bid of ${max_price}")
+                # return bid
+            elif max_count >= 2:
+                # state the value and run the silent auction again
+                print(f"\nThe maximum bid is tied, we would have to start all over\nFeel free to change your bid price")
+                bid = {}
+
+                # restart loop
+                continue_bid = True
+
+        elif other_bidders != "y":
             print(
                 "please type the appropriate response: \"y\" for yes and \"n\" for no: ")
             other_bidders
+        else: continue_bid = True
 
 
 # run function
 auction()
-
-# Get max price and key
-max_name = max(bid, key=bid.get)
-max_price = max(bid.values())
-
-# convert bid values to list
-max_count = list(bid.values()).count(max_price)
-
-if max_count >= 2:
-    # state the value and run the silent auction again
-    print(f"The maximum bid is tied, we would have to start all over\nFeel free to change your bid price")
-    # run function again
-    auction()
-elif max_count == 1:
-    print(f"The winner is {max_name} with a bid of ${max_price}")
-    # return bid
