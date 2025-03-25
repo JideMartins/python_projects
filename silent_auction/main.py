d14c569646135fde202c0a96232c6ada1088f2ef
JideMartins <jeedeyy@gmail.com>
2025-03-24 21:39:15 -0400

    declare variablea and dictionary

---
+++ silent_auction/main.py
@@ -0,0 +1,10 @@
+
+bid = {}
+
+# Ask for inputs
+name = input("Enter your name:\n")
+price = input("Enter your bidding price:\n$")
+
+# check if there are other bidders
+other_bidders = input("Are there other bidders?").lower()
+
